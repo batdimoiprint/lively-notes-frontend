308b949285cc7294f3b93c25b30a39e643d3d4b8
batdimoiprint <johnkennyreyes21@gmail.com>
2025-12-28 00:59:54 +0800

    feat: update axios instance to conditionally set baseURL based on production environment

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_ENV_BASE_URL,
+    baseURL: import.meta.env.PROD
+        ? import.meta.env.VITE_ENV_BASE_URL
+        : "http://localhost:3000",
     headers: { "Content-Type": "application/json" },
 });
 
