722e508a2631d898a5f269b0e473fecb98181f9c
batdimoiprint <johnkennyreyes21@gmail.com>
2026-01-04 22:38:15 +0800

    feat: add confirmation dialog for task deletion in TaskCard component

--- src/components/home/TasksGrid/TaskCard.tsx
+++ src/components/home/TasksGrid/TaskCard.tsx
@@ -1,70 +1,87 @@
 import {
-    Card,
-    CardAction,
-    CardContent,
-    CardHeader,
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
 } from "@/components/ui/card";
 import { SheetTrigger } from "@/components/ui/sheet";
 import { Spinner } from "@/components/ui/spinner";
 import type { Tasks } from "@/types/tasktypes";
 import { Label } from "@radix-ui/react-label";
 import type { UseMutationResult } from "@tanstack/react-query";
-import { X } from "lucide-react";
+import { Check, Trash, X } from "lucide-react";
+import { useState } from "react";
 
 interface TaskCardProps {
-    task: Tasks;
-    deletingId: string | null;
-    setDeletingId: React.Dispatch<React.SetStateAction<string | null>>;
-    setSelectedTask: React.Dispatch<React.SetStateAction<Tasks | null>>;
-    mutation: UseMutationResult<number | undefined, Error, string, unknown>;
-
+  task: Tasks;
+  deletingId: string | null;
+  setDeletingId: React.Dispatch<React.SetStateAction<string | null>>;
+  setSelectedTask: React.Dispatch<React.SetStateAction<Tasks | null>>;
+  mutation: UseMutationResult<number | undefined, Error, string, unknown>;
 }
 
 export default function TaskCard({
-    task,
-    deletingId,
-    mutation,
-    setDeletingId,
-    setSelectedTask,
+  task,
+  deletingId,
+  mutation,
+  setDeletingId,
+  setSelectedTask,
 }: TaskCardProps) {
-    return (
-        <>
-            <Card
-                key={task._id}
-                className="p-2 overflow-hidden max-h-48"
-                onClick={() => {
-                    setSelectedTask(task);
-                }}
-            >
-                <SheetTrigger>
-                    <CardHeader>
-                        {/* {task._id} */}
-                        <Label className="font-bold w-48 overflow-hidden text-ellipsis whitespace-nowrap">
-                            {task.title}
-                        </Label>
+  const [confirming, setConfirming] = useState<boolean>(false);
 
-                        <CardAction>
-                            <span
-                                onClick={async (e) => {
-                                    setDeletingId(task._id);
-                                    mutation.mutate(task._id);
-                                    e.stopPropagation();
-                                }}
-                                className="cursor-pointer"
+  function deleteTask() {
+    setDeletingId(task._id);
+    mutation.mutate(task._id);
+  }
 
-                            >
-                                {deletingId === task._id ? <Spinner /> : <X />}
-                            </span>
-                        </CardAction>
-                    </CardHeader>
-                    <CardContent
-                        className="whitespace-break-spaces "
+  return (
+    <>
+      <Card
+        key={task._id}
+        className="p-2 overflow-hidden max-h-48"
+        onClick={() => {
+          setSelectedTask(task);
+        }}
+      >
+        <SheetTrigger>
+          <CardHeader>
+            {/* {task._id} */}
+            <Label className="font-bold w-48 overflow-hidden text-ellipsis whitespace-nowrap">
+              {task.title}
+            </Label>
 
-                    >
-                        {task.body}
-                    </CardContent>
-                </SheetTrigger>
-            </Card>
-        </>
-    );
+            <CardAction>
+              <span className="cursor-pointer" onClick={e => e.stopPropagation()}>
+                {deletingId === task._id ? (
+                  <Spinner />
+                ) : confirming ? (
+                  <div className="flex flex-row gap-2">
+                        <Check
+                        onClick={() => {
+                            deleteTask();
+                            setConfirming(false);
+                        }}
+                        className="  cursor-pointer inline"
+                        />
+                        <X
+                        onClick={() => setConfirming(false)}
+                        className=" cursor-pointer inline"
+                        />
+                  </div>
+                ) : (
+                  <Trash
+                    onClick={() => setConfirming(true)}
+                    className="cursor-pointer"
+                  />
+                )}
+              </span>
+            </CardAction>
+          </CardHeader>
+          <CardContent className="whitespace-break-spaces ">
+            {task.body}
+          </CardContent>
+        </SheetTrigger>
+      </Card>
+    </>
+  );
 }
